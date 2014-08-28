35bf7f585d182d1193563ed96d4d2516f1b763a5
Yalin Wang <ywang28@ncsu.edu>
2014-08-27 23:44:24 -0700

    canvas basic shapes

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 onload = function() {
+	var canvas = document.getElementById('drawing');
+	var context = canvas.getContext('2d');
+	
+	context.lineWidth = 10;
+	context.strokeStyle = "#55f";
+	
+	context.moveTo(10,10);
+	context.lineTo(200,10);
+	context.stroke();
+	
+	context.beginPath();
+	context.moveTo(10,50);
+	context.lineTo(200,50);
+	context.lineCap = "round";
+	context.stroke();
+	
+	context.beginPath();
+	context.moveTo(40,100);
+	context.lineTo(200,100);
+	context.lineTo(120, 150);
+	context.closePath();
+	context.fillStyle = "blue";
+	context.fill();
+	context.strokeStyle = "red";
+	context.lineCap = "butt";
+	context.stroke();
+	
+	context.beginPath();
 };
