1c697c4ac6a641e139f015530f21edcbefcd6d3e
Yalin Wang <ywang28@ncsu.edu>
2014-09-08 10:35:12 -0700

    hiding sidebar

--- scripts/learn.js
+++ scripts/learn.js
@@ -1,2 +1,24 @@
-$(function	
-);
+$(function()  {
+	$('.topic').hover(
+		function()  {
+			$(this).animate(
+				{
+					left:  0,
+					backgroundColor:  '#ffd'
+				},
+				500,
+				'easeInSine'
+			);
+		},
+		function()  {
+			$(this).animate(
+				{
+					left:  -280,
+					backgroundColor:  '#5af'
+				},
+				1500,
+				'easeOutBounce'
+			);
+		}
+	);
+});
