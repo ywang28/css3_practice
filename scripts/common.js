6020f1f849d0889ef2f942d3e9339af919794558
Yalin Wang <ywang28@ncsu.edu>
2014-09-16 22:13:02 -0700

    popup win

--- scripts/common.js
+++
@@ -1,4 +0,0 @@
-var openExt = function()  {
-	// open external link in new window
-	$('a[href^="http://"]').attr('target','_blank');
-};
--- scripts/relocation.js
+++ scripts/relocation.js
@@ -1,4 +1,14 @@
 $(function()  {
 	// open external link in new window
-	openExt();
+	$('a.newwin').attr('target','_blank');
+	// popup window
+	$('a.popup').click(
+		function()  {
+			var winProp = "width=600, height=400, resizable=yes, scrollbar=yes, top=100, left=200";
+			var pop = open($(this).attr('href'), 'popWin', winProp);
+			return false;
+		}
+	);
+	// fancy box window
+	$('a.iframe').fancybox();
 });
